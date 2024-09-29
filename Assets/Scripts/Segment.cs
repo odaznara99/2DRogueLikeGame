6f0ff0b81a5a7889ab77a5494dd544db2907b554
odaznara99 <jrodaz66@gmail.com>
2024-09-29 15:43:32 +0800

    Added Random Generation of Levels

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -6,4 +6,12 @@ public class Segment : MonoBehaviour
 {
     public Transform startPoint;
     public Transform endPoint;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(startPoint.position, 0.5f);  // Start point of the segment
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(endPoint.position, 0.5f);    // End point of the segment
+    }
 }
--- Assets/Scripts/SegmentManager.cs
+++ Assets/Scripts/SegmentManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class SegmentManager : MonoBehaviour
@@ -7,16 +8,33 @@ public class SegmentManager : MonoBehaviour
     public GameObject[] segmentPrefabs;
     public Transform player;
     public int maxSegments = 5;
+
     private List<GameObject> activeSegments = new List<GameObject>();
     private Vector3 nextSpawnPoint;
 
     void Start()
     {
+        //Find the player position
+        if (player == null)
+        {
+            player = transform.Find("HeroKnight").GetComponent<Transform>();
+        }
+
+        //To avoid zero or null
+        if (maxSegments == 0) {
+            maxSegments = 5;
+        }
+
+        //Starting SpawnPoint
+        nextSpawnPoint = new Vector3(player.position.x-6,0,0);
+
         // Spawn initial segments
         for (int i = 0; i < maxSegments; i++)
         {
             SpawnSegment();
         }
+
+        
     }
 
     void Update()
@@ -30,8 +48,18 @@ public class SegmentManager : MonoBehaviour
 
     void SpawnSegment()
     {
-        int randomIndex = Random.Range(0, segmentPrefabs.Length);
+        //Spaw Random Segment
+        int randomIndex = Random.Range(0, segmentPrefabs.Length);      
         GameObject newSegment = Instantiate(segmentPrefabs[randomIndex], nextSpawnPoint, Quaternion.identity);
+
+        // Get the start point
+        Transform newSegmentStartPoint = newSegment.GetComponent<Segment>().startPoint;
+
+        // Offset the position
+        Vector3 offset = nextSpawnPoint - newSegmentStartPoint.position;
+        newSegment.transform.position += offset;
+
+        // Add the new segment to the list of active segments
         activeSegments.Add(newSegment);
 
         // Update next spawn point based on the end point of the new segment
@@ -50,6 +78,8 @@ public class SegmentManager : MonoBehaviour
     bool PlayerReachedNextSegment()
     {
         // Define how close the player must be to the end point to spawn the next segment
+        ////Debug.Log("Player position is:" + player.position.x);
+        ////Debug.Log("End point is:" + nextSpawnPoint.x);
         return player.position.x > nextSpawnPoint.x - 10f;
     }
 }
